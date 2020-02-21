500eebce06e490158e9055ca02754bc29896c0ef
Yuriy <yuriy_sh@zoho.com>
2020-02-21 03:42:00 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix) return [];
+  let newArr = [];
+  
+  for (let i = 0; i < matrix.length; i++) {
+    if((i+1)%2===0){
+      for (let y = matrix[i].length - 1; y >= 0 ; y--) {
+        newArr.push(matrix[i][y])
+      }
+    } else{
+      for (let y = 0; y < matrix[i].length; y++) {
+        newArr.push(matrix[i][y])
+      }
+    }
+    
+  }
+  return newArr;
 }
